522e811c4557a18a0c1b1b178e93e8385716b046
Yanis Bendahmane <yanis.bendahmane@supinternet.fr>
2018-01-10 15:09:49 +0100

    Little modifications

--- tp/tp.go
+++ tp/tp.go
@@ -15,7 +15,7 @@ func write(filename string, content[]byte){
 	ioutil.WriteFile(filename, content, 0644)
 }
  
-func concatenate(datas ...string) (sum []byte, err error){
+func concatenate(datas ...string) (sum []byte){
 	for i := 0; i < len(datas); i++{
 		f := read(datas[i])
 		sum = append(sum, f...)
@@ -28,6 +28,6 @@ func main() {
 		fmt.Println("I need at least 4 files!")
 		return
 	}
-	sum, _ := concatenate(os.Args[1], os.Args[2], os.Args[3])
+	sum := concatenate(os.Args[1], os.Args[2], os.Args[3])
 	write(os.Args[4], sum)
 }
