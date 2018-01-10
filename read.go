1e41b0eddcfdeab9af095b7477fd30ec40c17417
Yanis Bendahmane <yanis.bendahmane@supinternet.fr>
2018-01-10 12:16:28 +0100

    CONCATE WORKS ! YAY

--- read.go
+++ read.go
@@ -14,19 +14,39 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
 	var contentStr string
 	contentStr = string(content)
 	fmt.Println(contentStr)
 
-	var byteName []byte
-	byteName = []byte(name)
 	var writeInto string
 	writeInto = os.Args[2]
-	fmt.Println(writeInto)
 	err = ioutil.WriteFile(writeInto, content, 0777)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
+	var concatenateMe string
+	concatenateMe = os.Args[4]
+	var concatMe string
+	var data3 string
+	var bytedConcat []byte
+	var stringifiedData2 string
+	var stringifiedData3 string
+	data3 = os.Args[3]
+	content2, err := ioutil.ReadFile(writeInto)
+	content3, err := ioutil.ReadFile(data3)
+	stringifiedData2 = string(content2)
+	stringifiedData3 = string(content3)
+	concatMe = contentStr + stringifiedData2 + stringifiedData3
+	bytedConcat = []byte(concatMe)
+	err = ioutil.WriteFile(concatenateMe, bytedConcat, 0777)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func concatenate(data1 string, data2 string, data3 string) (sum string, err error){
+	return
 }
