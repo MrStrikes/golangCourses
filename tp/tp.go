52e9bbac892d4159ed35dc7a6b35427c7fcf7719
Yanis Bendahmane <yanis.bendahmane@supinternet.fr>
2018-01-10 15:02:24 +0100

    Corrected and finished the TP

--- tp/tp.go
+++ tp/tp.go
@@ -6,54 +6,28 @@ import (
 	"os"
 )
 
-func concatenate(data1 string, data2 string, data3 string) (sum string, err error){
-	var concatenateMe string
-	concatenateMe = os.Args[4]
-	var concatMe string
-	var bytedConcat []byte
-	var stringifiedData2 string
-	var stringifiedData3 string
-	data3 = os.Args[3]
-	content2, err := ioutil.ReadFile(data2)
-	content3, err := ioutil.ReadFile(data3)
-	stringifiedData2 = string(content2)
-	stringifiedData3 = string(content3)
-	concatMe = data1 + stringifiedData2 + stringifiedData3
-	bytedConcat = []byte(concatMe)
-	err = ioutil.WriteFile(concatenateMe, bytedConcat, 0777)
-	if err != nil {
-		fmt.Println(err)
-		return
+func read(filename string) (content []byte){
+	content, _ = ioutil.ReadFile(filename)
+	return
+}
+
+func write(filename string, content[]byte){
+	ioutil.WriteFile(filename, content, 0644)
+}
+ 
+func concatenate(datas ...string) (sum []byte, err error){
+	for i := 0; i < len(datas); i++{
+		f := read(datas[i])
+		sum = append(sum, f...)
 	}
 	return
 }
 
-
 func main() {
-	var name string
-	name = os.Args[1]
-	content, err := ioutil.ReadFile(name)
-	if err != nil {
-		fmt.Println(err)
+	if len(os.Args) < 5{
+		fmt.Println("I need at least 4 files!")
 		return
 	}
-	var contentStr string
-	contentStr = string(content)
-	fmt.Println(contentStr)
-
-	var writeInto string
-	writeInto = os.Args[2]
-	ioutil.WriteFile(writeInto, content, 0777)
-
-	var data3 string
-	data3 = os.Args[3]
-	content3, err := ioutil.ReadFile(data3)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	var content3Str string
-	content3Str = string(content3)
-	
-	concatenate(name, writeInto, content3Str)
+	sum, _ := concatenate(os.Args[1], os.Args[2], os.Args[3])
+	write(os.Args[4], sum)
 }
