c9391e7eae0dd5982f25cfe9d083f4432e1faa44
root <root@DESKTOP-V1EBRND.localdomain>
2018-01-12 14:54:42 +0100

    Can now read URL's

--- finalProject/aspire-contact/aspire-contact.go
+++ finalProject/aspire-contact/aspire-contact.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
-	urls, _ := ioutil.ReadFile("URL.txt")
-	stringUrls := string(urls)
-	fmt.Printf("HTML code of %s ...\n", urls)
-	resp, err := http.Get(stringUrls)
-	if err != nil {
-		panic(err)
+	inFile, _ := os.Open(os.Args[1])
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+	fmt.Printf("HTML code of %s \n", scanner.Text())
+	for scanner.Scan() {
+		resp, err := http.Get(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		html, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("%s\n", html)
 	}
 }
