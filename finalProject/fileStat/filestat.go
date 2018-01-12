fda31897ca6d5e56713e37f0e6d405b2d21be15c
root <root@DESKTOP-V1EBRND.localdomain>
2018-01-12 13:48:17 +0100

    Thank you Neil

--- finalProject/fileStat/filestat.go
+++ finalProject/fileStat/filestat.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
+	"unicode"
 )
 
 func main() {
@@ -18,11 +20,8 @@ func main() {
 	}
 	fmt.Println("The file is", fi.Size(), "bytes")
 	inFile, _ := ioutil.ReadFile("file.txt")
-	for i := 0; i < len(inFile); i++ {
-		runeFile := []rune(string(inFile[i]))
-		fmt.Println(runeFile)
-	}
-	fmt.Println(maping(inFile))
+	res := maping(inFile)
+	count(res)
 }
 
 func maping(byteContent []byte) map[rune]int {
@@ -31,6 +30,42 @@ func maping(byteContent []byte) map[rune]int {
 	for _, j := range runeContent {
 		myMap[j] = myMap[j] + 1
 	}
-	fmt.Println(myMap)
 	return myMap
+}
+
+func count(Map map[rune]int) {
+	var maxCount int
+	var maxLetter []rune
+	minCount := 99999999
+	var minLetter []rune
+	for i, j := range Map {
+		if unicode.IsLetter(i) {
+			if j > maxCount {
+				maxCount = j
+				maxLetter = []rune{i}
+			} else if j >= maxCount {
+				maxCount = j
+				maxLetter = append(maxLetter, i)
+			}
+			if j < minCount {
+				minCount = j
+				minLetter = []rune{i}
+			} else if j <= minCount {
+				minCount = j
+				minLetter = append(minLetter, i)
+			}
+		}
+	}
+	if len(minLetter) < 2 {
+		fmt.Printf("Least recurrent letter is %s with %d occurrences\n", strings.Split(string(minLetter), ""), minCount)
+	} else {
+		fmt.Printf("Least recurrent letters are %s with %d occurrences\n", strings.Split(string(minLetter), ""), minCount)
+	}
+
+	if len(maxLetter) < 2 {
+		fmt.Printf("Most recurrent letter is %s with %d occurrences\n", strings.Join(strings.Split(string(maxLetter), ""), ","), maxCount)
+	} else {
+		fmt.Printf("Most recurrent letters are %q with %d occurrences\n", strings.Join(strings.Split(string(maxLetter), ""), ", "), maxCount)
+	}
+
 }
