0abda2494b2c2f5dce98fd75ce7eaf4d3c3fc3f8
root <root@DESKTOP-V1EBRND.localdomain>
2018-01-12 12:49:32 +0100

    Now the content is mapped

--- finalProject/fileStat/filestat.go
+++ finalProject/fileStat/filestat.go
@@ -1,5 +1,36 @@
 package main
 
-import(
+import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
+
+func main() {
+	file, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("The file is", fi.Size(), "bytes")
+	inFile, _ := ioutil.ReadFile("file.txt")
+	for i := 0; i < len(inFile); i++ {
+		runeFile := []rune(string(inFile[i]))
+		fmt.Println(runeFile)
+	}
+	fmt.Println(maping(inFile))
+}
+
+func maping(byteContent []byte) map[rune]int {
+	runeContent := []rune(string(byteContent))
+	myMap := make(map[rune]int)
+	for _, j := range runeContent {
+		myMap[j] = myMap[j] + 1
+	}
+	fmt.Println(myMap)
+	return myMap
+}
