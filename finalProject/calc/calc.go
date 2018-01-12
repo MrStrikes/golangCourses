43797c38ff157959b8ce4efa194e7d32a508e2e0
root <root@DESKTOP-V1EBRND.localdomain>
2018-01-12 11:56:53 +0100

    Well, now it totally works

--- finalProject/calc/calc.go
+++ finalProject/calc/calc.go
@@ -1,70 +1,74 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strconv"
+	"fmt"
+	"os"
+	"strconv"
 )
 
-func mult(nums ...int)  {
-    result := 0
-    total := 1
-    for _, num := range nums {
-        result = total * num
-        total = result
-    }
-    fmt.Println(result)
-}
-
-func add(nums ...int){
+func mult(nums ...int) {
 	result := 0
-    total := 0
-    for _, num := range nums {
-        result = total + num
-        total = result
-    }
-    fmt.Println(result)
+	total := 1
+	for _, num := range nums {
+		result = total * num
+		total = result
+	}
+	fmt.Println(result)
 }
 
-func sub(nums ...int){
+func add(nums ...int) {
 	result := 0
-    total := 0
-    for _, num := range nums {
-        result = num - total
-        total = result
-    }
-    fmt.Println(result)
+	total := 0
+	for _, num := range nums {
+		result = total + num
+		total = result
+	}
+	fmt.Println(result)
 }
 
-func div(nums ...int){
+func sub(nums ...int) {
+	result := 0
+	total := 0
+	for _, num := range nums {
+		result = num - total
+		total = result
+	}
+	fmt.Println(result)
+}
+
+func div(nums ...int) {
 	result := 1
-    total := 1
-    for _, num := range nums {
-        result = num / total
-        total = result
-    }
-    fmt.Println(result)
+	total := 1
+	for _, num := range nums {
+		result = num / total
+		total = result
+	}
+	fmt.Println(result)
 }
 
 func main() {
-    var d [] int
-    var args= os.Args[1:]
-    nums := make([]int, len(args))
-    for i := 0; i < len(args); i++ {
-        nums[i], _ = strconv.Atoi(args[i]);
-        strconv.Atoi(args[i])
-        d = append(d, nums[i])
-    }
-    num := d
-    if os.Args[1] == "*"{
+	var d []int
+	var args = os.Args[1:]
+	nums := make([]int, len(args)-1)
+	for i := 0; i < len(args); i++ {
+		ret, errAtoi := strconv.Atoi(args[i])
+		if errAtoi != nil {
+			fmt.Println(errAtoi.Error())
+		} else {
+			nums[i-1] = ret
+			d = append(d, nums[i-1])
+		}
+	}
+	num := d
+	if os.Args[1] == "*" {
 		mult(num...)
-	} else if os.Args[1] == "+"{
+	} else if os.Args[1] == "+" {
 		add(num...)
-	} else if os.Args[1] == "-"{
+	} else if os.Args[1] == "-" {
 		sub(num...)
-	} else if os.Args[1] == "/"{
+	} else if os.Args[1] == "/" {
 		div(num...)
-    } else {
-        fmt.Println("Well well well, you didn't entered a right operand ! Try with +, -, /, or * between double quotes")
-    }
+	} else {
+		fmt.Println("Well well well, you didn't entered a right operand ! Try with +, -, /, or * between double quotes")
+	}
 }
